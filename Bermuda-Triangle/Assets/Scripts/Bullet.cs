44fd41ec3eb39eafc9218b30e26448bffd553c44
dresio <dresiomob@gmail.com>
2016-10-01 00:39:19 -0400

    Add game and basic functions
    Initializes all game files, adds the ability to move and shoot.  Also
    has the start to the melee attack.

---
+++ Bermuda-Triangle/Assets/Scripts/Bullet.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+ * Created by Daniel Resio
+ * Handles bullet movement and duration
+ **/
+public class Bullet : MonoBehaviour {
+
+    #region public variables
+    private Vector3 direction;
+    private int duration;
+    private int speed = 7;
+    #endregion
+
+    //sets to 0 temporarily to avoid errors
+    void Start()
+    {
+        //finds mouse position
+        Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 charPos = GameObject.FindWithTag("Player").transform.position;
+        Vector2 dif = -(charPos - pos);
+        //normalizes the vector
+        direction = dif.normalized;
+        //decreases speed
+        direction.x /= speed;
+        direction.y /= speed;
+        //sets length that the bullet will fly
+        duration = 50;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log("bullet position = " + gameObject.transform.position);
+        //moves object towards vector until duration is up
+        if (!(duration < 0))
+        {
+            gameObject.transform.Translate(direction);
+            duration -= 1;
+            return;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
